53c57105e9c7a14200862446392e6e1c798c7721
Alexander Pavlovets <sam-90@bigmir.net>
2019-01-09 19:29:35 +0200

    GUI related refactor. interpolation and base templates added

--- node_wo_frameworks/app/lib/helpers.js
+++ node_wo_frameworks/app/lib/helpers.js
@@ -10,6 +10,8 @@ const crypto = require('crypto')
 const config = require('./config')
 const https = require('https')
 const querystring = require('querystring')
+const path = require('path')
+const fs = require('fs')
 
 // Container for all helpers
 const helpers = {}
@@ -112,6 +114,73 @@ helpers.sendTwilioSms = function(phone, msg, callback) { // this won't work, bec
   } else {
     callback('Given parameters were missing or invalid')
   }
+}
+
+// Get the string content of a template
+helpers.getTemplate = function (templateName, data, callback) {
+  templateName = typeof(templateName) === 'string' && templateName.length > 0 ? templateName : false
+  data = typeof(data) === 'object' && data !== null ? data : {}
+  if(templateName) {
+    const templatesDir = path.join(__dirname, '../templates/')
+    fs.readFile(templatesDir + templateName + '.html', 'utf-8', function(err, str) {
+      if(!err && str && str.length > 0) {
+        // Do interpolation on the string
+        const finalString = helpers.interpolate(str, data)
+        callback(false, finalString)
+      } else {
+        callback('No template could be found')
+      }
+    })
+  } else {
+    callback('A valid template name was not specified')
+  }
+}
+
+// Add the universal header and footer to a string, and pass provided data object to the header and footer for interpolation
+helpers.addUniversalTemplates = function(str, data, callback) {
+  str = typeof(str) === 'string' && str.length > 0 ? str : ''
+  data = typeof(data) === 'object' && data !== null ? data : {}
+  // Get the header
+  helpers.getTemplate('_header', data, function(err, headerString){
+    if(!err && headerString) {
+      // Get the footer
+      helpers.getTemplate('_footer', data, function(err, footerString) {
+        if(!err && footerString) {
+          // Add them all together
+          const fullString = headerString + str + footerString
+          callback(false, fullString)
+        } else {
+          callback('Could not find the footer template')
+        }
+      })
+    } else {
+      callback('Could not find the header template')
+    }
+  })
+}
+
+// Take a given string and a data object and find/replace all the keys within it
+helpers.interpolate = function(str, data) {
+  str = typeof(str) === 'string' && str.length > 0 ? str : ''
+  data = typeof(data) === 'object' && data !== null ? data : {}
+
+  // Add the templateGlobals to the data object, prepending their key name with "global"
+  for (const keyName in config.templateGlobals) {
+    if(config.templateGlobals.hasOwnProperty(keyName)) {
+      data['global.' + keyName] = config.templateGlobals[keyName]
+    }
+  }
+
+  // For each key in the data object, insert it's value inte the string at the coresponding placeholder
+  for(const key in data) {
+    if(data.hasOwnProperty(key) && typeof(data[key]) === 'string') {
+      const replace = data[key]
+      const find = '{' + key + '}'
+      str = str.replace(find, replace)
+    }
+  }
+
+  return str
 }
 
 // Export the module
