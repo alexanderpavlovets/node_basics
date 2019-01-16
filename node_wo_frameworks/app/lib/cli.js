7df78287c6c7299455c7fb4e6a8fe70bc8b4e9f7
Alexander Pavlovets <sam-90@bigmir.net>
2019-01-16 13:08:57 +0200

    child process example is added

--- node_wo_frameworks/app/lib/cli.js
+++ node_wo_frameworks/app/lib/cli.js
@@ -15,6 +15,7 @@ const v8 = require('v8')
 const _data = require('./data')
 const _logs = require('./logs')
 const helpers = require('./helpers')
+const childProcess = require('child_process')
 
 
 // Instantiate the CLI module
@@ -276,16 +277,18 @@ cli.responders.moreCheckInfo = function(str) {
 
 // List logs
 cli.responders.listLogs = function() {
-  _logs.list(true, function(err, logFileNames) {
-    if(!err && logFileNames && logFileNames.length > 0) {
-      cli.verticalSpace()
-      logFileNames.forEach(function(logFileName) {
-        if(logFileName.indexOf('-') > -1) { // only compressed logs
-          console.log(logFileName)
-          cli.verticalSpace()
-        }
-      })
-    }
+  const ls = childProcess.spawn('ls', ['./.logs/'])
+  ls.stdout.on('data', function(dataObject) {
+    // Explode into separate lines
+    const dataStr = dataObject.toString()
+    const logFileNames = dataStr.split('\n')
+    cli.verticalSpace()
+    logFileNames.forEach(function(logFileName) {
+      if(typeof(logFileName) === 'string' && logFileName.length > 0 && logFileName.indexOf('-') > -1) { // indexOf('-') is for only compressed logs
+        console.log(logFileName.trim().split('.')[0]) // log and remove extension
+        cli.verticalSpace()
+      }
+    })
   })
 }
 
