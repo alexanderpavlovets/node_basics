609638dbaead15ac06e1e3c35edcda359bec2c5a
Alexander Pavlovets <sam-90@bigmir.net>
2019-01-25 12:53:53 +0200

    Just piece of code

--- specs/one.ts
+++ specs/one.ts
@@ -1,24 +1,10 @@
-import { browser, $, $$, element, by, ExpectedConditions as ES } from 'protractor'
+import {browser, $, $$, element, by, ExpectedConditions as ES} from 'protractor'
 
 describe('First suite', () => {
-    it('One is equal to one', wrappCB(async () => {
-        await browser.get('')
-        await browser.sleep(2000)
-        throw new Error('Error thrown')
-        expect(1).toBe(1, '1 should be equal to 1, smth went really wrong')
-    }))
+  it('One is equal to one', async () => {
+    await browser.get('')
+    await browser.sleep(2000)
+
+    expect(1).toBe(1, '1 should be equal to 1, smth went really wrong')
+  })
 })
-
-
-function wrappCB(fn) {
-
-  return async function() {
-    try {
-      await fn()
-    } catch (e) {
-      if (e) {
-        console.log('Catched error')
-      }
-    }
-  }
-}
