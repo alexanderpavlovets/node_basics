22404a8fc2646169d2055b8c8ef5781ead473738
Alexander <user@Administrators-MacBook-Pro.local>
2019-03-05 10:51:18 +0200

    typescript updated

--- specs/one.ts
+++ specs/one.ts
@@ -7,25 +7,4 @@ describe('First suite', () => {
 
     expect(1).toBe(1, '1 should be equal to 1, smth went really wrong')
   })
-
-  // it('isPresent for $ and $$', async () => {
-  //   await browser.get('')
-  //   await browser.sleep(2000)
-
-  //   const fields = $$('.ng-scope')
-  //   console.log(await fields.count()) // 18 elements
-
-  //   // Removing one of elements
-  //   const thirdField = fields.get(3)
-  //   await browser.executeScript(`
-  //     arguments[0].remove()
-  //   `, thirdField.getWebElement())
-
-
-  //   console.log(await fields.count()) // 17 elements
-  //   console.log(await fields.isPresent())
-  //   console.log(await fields.first().isPresent())
-
-  //   expect(1).toBe(1, '1 should be equal to 1, smth went really wrong')
-  // })
 })
