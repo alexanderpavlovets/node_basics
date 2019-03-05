d398ddda8d9269f78b38fe9f09e1e7440f047f4c
Alexander Pavlovets <olpav@ciklum.com>
2019-03-05 19:38:33 +0200

    isPrimitive check is added

---
+++ playground/isPrimitiveCheck.js
@@ -0,0 +1,11 @@
+
+const isArgumentPrimitive = (arg) => (arg !== Object(arg))
+
+console.log(isArgumentPrimitive(123))       // true 
+console.log(isArgumentPrimitive('123'))     // true 
+console.log(isArgumentPrimitive(null))      // true 
+console.log(isArgumentPrimitive(undefined)) // true 
+console.log(isArgumentPrimitive({}))        // false 
+console.log(isArgumentPrimitive([]))        // false 
+console.log(isArgumentPrimitive(true))      // true 
+console.log(isArgumentPrimitive(false))     // true 
