9889dd3c336e4c68502745905b24e8360670bfa3
Alexander Pavlovets <alexanderpavlovets@Alexanders-MacBook-Pro.local>
2019-03-14 14:36:47 +0200

    Object prototype playground is modified

--- playground/obj.prototype.js
+++ playground/obj.prototype.js
@@ -9,3 +9,17 @@ console.log(parent.__proto__)
 console.log(child.prototype)
 console.log(child.__proto__)
 console.log(child.a)
+
+// ___________________
+console.log('________________')
+
+function ConstructorFunction() {
+  this.name = 'Alex'
+}
+
+const instance = new ConstructorFunction()
+
+console.log(instance)
+console.log(instance.prototype) // undefined
+console.log(instance.__proto__)
+console.log(instance.constructor.prototype === instance.__proto__) // true
