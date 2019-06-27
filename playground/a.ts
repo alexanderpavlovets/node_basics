af8822589c94cab8e8d6beb649cd8e24381a45ba
Alexander Pavlovets <olpav@ciklum.com>
2019-06-27 12:46:46 +0300

    Small prot inheritance example is added

--- playground/a.ts
+++ playground/a.ts
@@ -1,20 +1,48 @@
-
-const a = 'asd'
-const b = 123
-const c = {}
-const asd = true
-const asdf = function () {}
-const asdasd = null
-const aasdadasddas = undefined
-
-
-const human = {
-  name: 'Alex',
-  secondName: 'Smith',
-  age: 10
+enum Categories {
+  seasonal = 'Seasonal',
+  seasonalDecoration = 'Seasonal Decoration',
+  xmasDecoration = 'Xmas Decoration',
+  adventCalendars = 'Advent Calendars',
+  adventCalendarPreFilled = 'Advent Calendar Pre-Filled',
+  adventCalendarsFabric = 'Advent Calendars Fabric',
+  adventCalendarsWooden = 'Advent Calendars Wooden'
 }
 
-console.log(human.name)
-export {}
+const myPerfectObj = {
+  categoriesTree: {
+    seasonal: {
+      itemToClick: {name: null},
+      seasonalDecoration: {
+        itemToClick: {name: null},
+        xmasDecoration: {
+          itemToClick: {name: null},
+          adventCalendars: {
+            itemToClick: {name: null},
+            adventCalendarPreFilled: {
+              itemToClick: {details: null},
+            }
+          }
+        }
+      }
+    }
+  }
+}
 
+for (const [key, value] of Object.entries(myPerfectObj)) {
+  if(key === 'categoriesTree') {
+    asd(value)
+  }
+}
+
+
+
+function asd(cat) {
+
+  const a = Object.keys(Categories)
+  console.log(Object.keys(cat))
+
+  console.log(Object.values(cat))
+  console.log(...Object.values(cat))
+  
+}
 // to run TS use:  npm run ts-node ./playground/a.ts 
--- playground/b.js
+++ playground/b.js
@@ -1,1 +1,8 @@
 // placeholder
+
+
+
+const arr = [10,11,4,123]
+
+console.log(arr.sort((a, b) => b - a)) // descending [ 123, 11, 10, 4 ]
+console.log(arr.sort((a, b) => a - b)) // accending [ 4, 10, 11, 123 ]
