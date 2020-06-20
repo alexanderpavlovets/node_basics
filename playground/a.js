00e01709bef3ce8ef14b189508302cea1722b1f7
Alexander Pavlovets <aleksandrpavlovets90@gmail.com>
2020-06-20 10:17:32 +0300

    test multiple emails commi

--- playground/a.js
+++
@@ -1,7 +0,0 @@
-// placeholder
-
-const ob = { isMatch3: false, isRaid: true, isAttack: true }
-
-const a = Object.entries(ob).filter(action => action)
-
-console.log(a)
--- playground/b.js
+++ playground/b.js
@@ -1,1 +1,6 @@
 // placeholder
+
+const arr = [1,2,3,4]
+
+console.log(arr[1])
+console.log(arr[50])
